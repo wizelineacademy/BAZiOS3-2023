c5264d26b0bc0aae92e46642a5ab867a66d87d7e
vn55e3v <vn55e3v@mx.wal-mart.com>
2023-02-28 17:44:24 -0600

    Reference and sum are presented in the same line

--- curso/semana_6/sesion_1/Closures_and_value_types.playground/Contents.swift
+++ curso/semana_6/sesion_1/Closures_and_value_types.playground/Contents.swift
@@ -5,54 +5,10 @@ import UIKit
  To begin, closures capture references to variables.
  */
 
-func runClosure(_ closure: @escaping () -> Void) {
-    print("Running closure")
-    closure()
-}
-
-class Foo {
-    var x = 5
-}
-
-struct Bar: Equatable {
-    var x = 5
-}
-
-var foo = Foo()
-var bar = Bar()
-var x: Int = 5
-
-withUnsafePointer(to: x) {
-    print("1: \(x) has address: \($0)")
-}
-
-let closure1 = { () in
-    withUnsafePointer(to: x) {
-        print("2: \(x) has address: \($0)")
-    }
-    
-    print(x)
-    print("---")
-}
-
-runClosure(closure1)
-
-x = 6
-foo.x = 6
-bar.x = 6
-
-runClosure(closure1)
-runClosure(closure1)
-runClosure(closure1)
-runClosure(closure1)
-runClosure(closure1)
-
-print("Calculator example:____:")
-
 struct Calculator {
     var a: Int
     var b: Int
-    
+
     var sum: Int {
         return a + b
     }
@@ -61,85 +17,88 @@ struct Calculator {
 class Additioner {
     var a: Int
     var b: Int
-    
+
     init(a: Int, b: Int) {
         self.a = a
         self.b = b
     }
-    
+
     var sum: Int {
         return a + b
     }
 }
 
+func runClosure(_ closure: @escaping () -> Void) {
+    print("Running closure")
+    DispatchQueue.global().asyncAfter(deadline: .now() + 0.2) {
+        closure()
+    }
+}
 
 func calculate() {
     var calculator = Calculator(a: 3, b: 5)
+
     withUnsafePointer(to: calculator) {
         print("1 Calculator address", $0)
     }
-    
+
     let closure = { [calculator] in
-        //    calculator = Calculator(a: 33, b: 55) // 0x610000221be0
-        print(calculator.sum)
+//        calculator = Calculator(a: 33, b: 55)
         withUnsafePointer(to: calculator) {
-            print("2 Calculator address", $0)
+            print("2 Calculator address", $0, "Sum is: ", calculator.sum)
         }
     }
-    
+
     withUnsafePointer(to: calculator) {
-        print("3 Calculator address", $0)
+        print("3 Calculator address", $0, "Sum is: ", calculator.sum)
     }
-    
+
     calculator = Calculator(a: 3, b: 3)
     closure()
+
     withUnsafePointer(to: calculator) {
-        print("4 Calculator address", $0)
-        print(calculator.sum)
+        print("4 Calculator address", $0, "Sum is: ", calculator.sum)
     }
-    
+
     calculator = Calculator(a: 3, b: 11)
     withUnsafePointer(to: calculator) {
-        print("5 Calculator address", $0)
-        print(calculator.sum)
+        print("5 Calculator address", $0, "Sum is: ", calculator.sum)
     }
 }
 
 func add() {
     var additioner = Additioner(a: 3, b: 5)
-    
+
     withUnsafePointer(to: additioner) {
         print("1 Additioner address", $0)
     }
 
     let closure = { [unowned additioner] in
-        //        additioner = Calculator(a: 33, b: 55) // 0x610000221be0
-        print(additioner.sum)
+//        calculator = Calculator(a: 33, b: 55)
         withUnsafePointer(to: additioner) {
-            print("2 Additioner address", $0)
+            print("2 Additioner address", $0, "Sum is: ", additioner.sum)
         }
     }
 
     withUnsafePointer(to: additioner) {
-        print("3 Additioner address", $0)
+        print("3 Additioner address", $0, "Sum is: ", additioner.sum)
     }
+
     closure()
+
     additioner = Additioner(a: 3, b: 3)
-    //    closure()
+
+
     withUnsafePointer(to: additioner) {
-        print("4 Additioner address", $0)
-        print(additioner.sum)
+        print("4 Additioner address", $0, "Sum is: ", additioner.sum)
     }
-    
+
     additioner = Additioner(a: 3, b: 11)
     withUnsafePointer(to: additioner) {
-        print("5 Additioner address", $0)
-        print(additioner.sum)
+        print("5 Additioner address", $0, "Sum is: ", additioner.sum)
     }
 }
 
 calculate()
 print("----------------------------------------")
 add()
-
-
